876eb36015f569c044bb1ecdddeab8e559fc4efe
Denis Georgiev <dgeorgiev.web@gmail.com>
2020-08-15 11:04:28 +0300

    add entityframework support

---
+++ WishList/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WishList.Data.Models;
+
+namespace WishList.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Item> Items { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
